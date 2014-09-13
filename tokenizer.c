d8ece1c7cc101d87ffa39a7e1b050b3f6c276de6
Harshil Shah <harshilhshah1995@gmail.com>
2014-09-13 16:00:43 -0400

    tokenizer

---
+++ tokenizer.c
@@ -0,0 +1,91 @@
+/*
+ * tokenizer.c
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+ * Tokenizer type.  You need to fill in the type as part of your implementation
+ */
+
+struct TokenizerT_ {
+  int numTok;
+  char** toks;
+};
+
+typedef struct TokenizerT_ TokenizerT;
+
+/*
+ * TKCreate creates a new TokenizerT object for a given set of separator
+ * characters (given as a string) and a token stream (given as a string).
+ * 
+ * TKCreate should copy the two arguments so that it is not dependent on
+ * them staying immutable after returning.  (In the future, this may change
+ * to increase efficiency.)
+ *
+ * If the function succeeds, it returns a non-NULL TokenizerT.
+ * Else it returns NULL.
+ *
+ * You need to fill in this function as part of your implementation.
+ */
+
+TokenizerT *TKCreate(char *separators, char *ts) {
+
+  TokenizerT* tok = (TokenizerT *)malloc(sizeof(TokenizerT));
+  return NULL;
+}
+
+/*
+ * TKDestroy destroys a TokenizerT object.  It should free all dynamically
+ * allocated memory that is part of the object being destroyed.
+ *
+ * You need to fill in this function as part of your implementation.
+ */
+
+void TKDestroy(TokenizerT *tk) {
+}
+
+/*
+ * TKGetNextToken returns the next token from the token stream as a
+ * character string.  Space for the returned token should be dynamically
+ * allocated.  The caller is responsible for freeing the space once it is
+ * no longer needed.
+ *
+ * If the function succeeds, it returns a C string (delimited by '\0')
+ * containing the token.  Else it returns 0.
+ *
+ * You need to fill in this function as part of your implementation.
+ */
+
+char *TKGetNextToken(TokenizerT *tk) {
+  
+  return NULL;
+}
+
+/*
+ * main will have two string arguments (in argv[1] and argv[2]).
+ * The first string contains the separator characters.
+ * The second string contains the tokens.
+ * Print out the tokens in the second string in left-to-right order.
+ * Each token should be printed on a separate line.
+ */
+
+int main(int argc, char **argv) {
+
+  if(argc != 3){
+    fprintf(stderr,"Error: Incorrect number of arguments.\n");
+    return 1;
+  }
+  if(strcmp(argv[1],"-h") == 0){
+    printf("Usage: ./tokenizer <delimeters> <tokens>\n");
+    return 1;
+  }
+
+  TokenizerT* tok;
+  char* nextTok;
+  
+  tok = TKCreate(argv[1], argv[2]);
+
+  return 0;
+}
